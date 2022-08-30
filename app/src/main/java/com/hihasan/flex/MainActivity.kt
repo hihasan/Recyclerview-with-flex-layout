6b6b8565f0ffd9222613703be74c40b0a84e93a2
hihasan <hihasan07@gmail.com>
2022-08-31 01:55:30 +0600

    Code Push

--- app/src/main/java/com/hihasan/flex/MainActivity.kt
+++ app/src/main/java/com/hihasan/flex/MainActivity.kt
@@ -1,24 +1,34 @@
 package com.hihasan.flex
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import android.view.Menu
-import android.view.MenuItem
+import androidx.appcompat.app.AppCompatDelegate
+import com.google.android.flexbox.FlexDirection
+import com.google.android.flexbox.FlexboxLayoutManager
+import com.google.android.material.snackbar.Snackbar
 import com.hihasan.flex.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
+
+//    var dataModel: ArrayList<DataModel> = ArrayList()
+    lateinit var dataAdapter: DataAdapter
+
+    var dataModels = arrayListOf(
+        DataModel("Tv Shows"),
+        DataModel("fridge"),
+        DataModel("Sony"),
+        DataModel("Samsung Tv"),
+        DataModel("Lorem Ipsum this a test"),
+        DataModel("Coca Cola"),
+        DataModel("Pepsi"),
+        DataModel("7-up")
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
@@ -27,6 +37,22 @@ class MainActivity : AppCompatActivity() {
         binding.fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
+        }
+
+        initViews()
+
+
+    }
+
+    private fun initViews() {
+
+        val linearLayoutManager = FlexboxLayoutManager(applicationContext)
+        linearLayoutManager.flexDirection = FlexDirection.ROW
+        dataAdapter = DataAdapter(dataModels, applicationContext)
+
+        binding.content.dataRv.apply {
+            layoutManager = linearLayoutManager
+            adapter = dataAdapter
         }
     }
 
